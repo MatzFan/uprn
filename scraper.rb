b9c98c47f6f61eddaab1170f12a7dd1f1cb1397a
Bruce Steedman <bruce.steedman@gmail.com>
2014-06-21 23:20:50 +0100

    Scraper now uses Mechanizer to page forward to address search page and return results of example search at root

--- scraper.rb
+++ scraper.rb
@@ -5,17 +5,37 @@ class Scraper
   URL = 'https://www.dataprotection.gov.je/cms/Notification/'
   ID_FIELD = :'_ctl0:cphContent:InitialPage:txt_company_id'
   CODE_FIELD = :'_ctl0:cphContent:InitialPage:txt_security_code'
+  ADDRESS_SEARCH_TXT_FIELD = :'_ctl0:cphContent:CompanyDetail:lpi_contact_address:txt_search'
+  RESULTS_CSS = 'select#_ctl0_cphContent_CompanyDetail_lpi_contact_address_lb_results'
+  ID = '19961'
+  CODE = 'e6m6h6'
 
   def initialize
     @agent = Mechanize.new
   end
 
-  def get_login_page_source(company_id, code)
+  def get_login_page
     login_page = @agent.get(URL)
-    login_form = login_page.form('aspnetForm')
-    login_form.send(ID_FIELD,company_id) # use send as method (form field) name can be arbitrary string
-    login_form.send(CODE_FIELD, code) # ditto
-    page = @agent.submit(login_form, login_form.buttons.first).body
+    form = login_page.form('aspnetForm')
+    form.send(ID_FIELD, ID) # use send as method (form field) name can be arbitrary string
+    form.send(CODE_FIELD, CODE) # ditto
+    @agent.submit(form, form.buttons.first)
+  end
+
+  def get_page_5
+    form = get_login_page.form('aspnetForm')
+    @agent.submit(form, form.buttons.last)
+  end
+
+  def get_page_6
+    form = get_page_5.form('aspnetForm')
+    @agent.submit(form, form.buttons.last)
+  end
+
+  def get_uprns(search_string)
+    form = get_page_6.form('aspnetForm')
+    form.send(ADDRESS_SEARCH_TXT_FIELD, search_string)
+    @agent.submit(form, form.buttons.first).search(RESULTS_CSS).css('option').to_s
   end
 
 end
--- web.rb
+++ web.rb
@@ -4,5 +4,5 @@ require_relative './scraper'
 
 get '/' do
   s = Scraper.new
-  s.get_login_page_source('19961', 'e6m6h6')
+  s.get_uprns('station house')
 end
