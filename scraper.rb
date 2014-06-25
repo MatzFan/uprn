2cd03ca6211c8880dcf393abe20950b7b0f795d8
Bruce Steedman <bruce.steedman@gmail.com>
2014-06-25 20:03:31 +0100

    Add method to get an address from the search results and display it at root

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,8 @@ class Scraper
   ID_FIELD = :'_ctl0:cphContent:InitialPage:txt_company_id'
   CODE_FIELD = :'_ctl0:cphContent:InitialPage:txt_security_code'
   ADDRESS_SEARCH_TXT_FIELD = :'_ctl0:cphContent:CompanyDetail:lpi_contact_address:txt_search'
-  RESULTS_CSS = 'select#_ctl0_cphContent_CompanyDetail_lpi_contact_address_lb_results'
+  RESULTS_ID = 'select#_ctl0_cphContent_CompanyDetail_lpi_contact_address_lb_results'
+  ADDRESS_ID = 'select#_ctl0_cphContent_CompanyDetail_lpi_contact_address_txt_address'
   ID = '19961'
   CODE = 'e6m6h6'
 
@@ -32,10 +33,26 @@ class Scraper
     @agent.submit(form, form.buttons.last)
   end
 
-  def get_uprns(search_string)
+  def results_page_for(search_string)
     form = get_page_6.form('aspnetForm')
     form.send(ADDRESS_SEARCH_TXT_FIELD, search_string)
-    @agent.submit(form, form.buttons.first).search(RESULTS_CSS).css('option').to_s
+    @agent.submit(form, form.buttons.first)
+  end
+
+  def get_uprns(search_string)
+    results_page_for(search_string).search(RESULTS_ID).css('option').to_s
+  end
+
+  def get_address(search_string, num)
+    results_page = results_page_for(search_string)
+    form = results_page.form('aspnetForm')
+    form.field_with(name: '_ctl0:cphContent:CompanyDetail:lpi_contact_address:lb_results').options[num].select
+    form.add_field!('__EVENTTARGET', '_ctl0$cphContent$CompanyDetail$lpi_contact_address$lb_results')
+    new_page = @agent.submit(form)
+    new_page.search('#_ctl0_cphContent_CompanyDetail_lpi_contact_address_txt_address').children.to_s
   end
 
 end
+
+s = Scraper.new
+puts s.get_address('crabbe', 4)
--- web.rb
+++ web.rb
@@ -4,5 +4,6 @@ require_relative './scraper'
 
 get '/' do
   s = Scraper.new
-  s.get_uprns('station house')
+  # s.get_uprns('crabbe')
+  s.get_address('crabbe', 4)
 end
